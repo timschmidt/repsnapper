00565910a7af3dd3949d377bb34cdbff0c8018f7
Palomides <nave.notnilc@gmail.com>
2010-12-08 16:58:45 -0500

    more work on the OS X .app bundle

--- src/repsnapper-osx.sh
+++ src/repsnapper-osx.sh
@@ -1,4 +1,15 @@
-#!/bin/sh
-DYLD_FALLBACK_LIBRARY_PATH=./lib
+#!/bin/bash
 
-./repsnapper
+# get actual path to .app
+script_dir_link=$(dirname "$(readlink "$0")")
+if [[ $script_dir_link == "." ]]; then
+APP_PATH=$(dirname "$0")
+else
+APP_PATH=$script_dir_link
+fi
+
+# set place to load dylibs from
+DYLD_FALLBACK_LIBRARY_PATH=$APP_PATH/lib
+
+# run the program itself
+$APP_PATH/repsnapper
