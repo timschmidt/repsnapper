80a292d88247227a1b8f679dd75b3f58044da63f
Michael Meeks <michael.meeks@novell.com>
2011-08-30 14:22:44 +0100

    switch to perl autogen.sh that warns of needing pkg-config

--- autogen.sh
+++ autogen.sh
@@ -1,57 +1,163 @@
-#!/bin/sh
-# Run this to generate all the initial makefiles, etc.
+:
+    eval 'exec perl -S $0 ${1+"$@"}'
+        if 0;
 
-if test "z$1" = "z--clean"; then
-    echo "Cleaning"
+use strict;
 
-    rm -Rf autom4te.cache
-    rm -f missing install-sh mkinstalldirs libtool ltmain.sh
-    exit 1;
-fi
-
-requote()
+sub clean()
 {
-    out=""
-    for param in "$@" ; do
-	p=`echo "$param" | sed "s/'/'\\\\\\''/g"`
-	if test -z "$out" ; then
-	    out="'$p'"
-	else
-	    out="$out '$p'"
-	fi
-    done
-    echo "$out"
+    system ("rm -Rf autom4te.cache");
+    system ("rm -f missing install-sh mkinstalldirs libtool ltmain.sh");
+    print "cleaned the build tree\n";
 }
 
-old_args=""
-if test $# -eq 0 && test -f autogen.lastrun; then
-    old_args=$(cat autogen.lastrun)
-    echo "re-using arguments from last configure: $old_args";
-fi
+my $aclocal;
 
-touch ChangeLog
+# check we have various vital tools
+sub sanity_checks($)
+{
+    my $system = shift;
+    my @path = split (':', $ENV{'PATH'});
+    my %required =
+      (
+       'pkg-config' => "pkg-config is required to be installed",
+       'autoconf'   => "autoconf is required",
+       $aclocal     => "$aclocal is required",
+      );
 
-if test "z$ACLOCAL_FLAGS" = "z" -a "z`uname -s`" = "zDarwin" ; then
-    ACLOCAL_FLAGS="-I ./m4/mac"
-fi
-if test "z`uname -s`" != "zDarwin" ; then
-    AUTOMAKE_EXTRA_FLAGS=--warnings=no-portability
-fi
+    for my $elem (@path) {
+        for my $app (keys %required) {
+            if (-f "$elem/$app") {
+                delete $required{$app};
+            }
+        }
+    }
+    if ((keys %required) > 0) {
+        print ("Various low-level dependencies are missing, please install them:\n");
+        for my $app (keys %required) {
+            print "\t $app: " . $required{$app} . "\n";
+        }
+        exit (1);
+    }
+}
 
-conf_args=$(requote "$@")
+# one argument per line
+sub read_args($)
+{
+    my $file = shift;
+    my $fh;
+    my @lst;
+    open ($fh, $file) || die "can't open file: $file";
+    while (<$fh>) {
+        chomp();
+        # migrate from the old system
+        if ( substr($_, 0, 1) eq "'" ) {
+            print "Migrating options from the old autogen.lastrun format, using:\n";
+            my @opts;
+            @opts = split(/'/);
+            foreach my $opt (@opts) {
+                if ( substr($opt, 0, 1) eq "-" ) {
+                    push @lst, $opt;
+                    print "  $opt\n";
+                }
+            }
+        } elsif ( substr($_, 0, 1) eq "#" ) {
+            # comment
+        } else {
+            push @lst, $_;
+        }
+    }
+    close ($fh);
+    # print "read args from file '$file': @lst\n";
+    return @lst;
+}
 
-rm -f configure config.status config.h
-autoreconf -i -f
-aclocal $ACLOCAL_FLAGS || exit 1;
-intltoolize --copy --force --automake || exit 1;
-if test "x$NOCONFIGURE" = "x"; then
-    if test -n "$old_args" ; then
-        eval `echo ./configure $old_args`
-    else
-        echo "$(requote "$@")" > autogen.lastrun
-        echo "./configure ${conf_args}"
-        eval `echo ./configure ${conf_args}`
-    fi
-else
-    echo "Skipping configure process."
-fi
+sub invalid_distro($$)
+{
+    my ($config, $distro) = @_;
+    print STDERR "Can't find distro option set: $config\nThis is not necessarily a problem.\n";
+    print STDERR "Distros with distro option sets are:\n";
+    my $dirh;
+    opendir ($dirh, "distro-configs");
+    while (($_ = readdir ($dirh))) {
+        /(.*)\.conf$/ || next;
+        print STDERR "\t$1\n";
+    }
+    closedir ($dirh);
+}
+
+my @cmdline_args = ();
+if (!@ARGV) {
+    my $lastrun = "autogen.lastrun";
+    @cmdline_args = read_args ($lastrun) if (-f $lastrun);
+} else {
+    @cmdline_args = @ARGV;
+}
+
+my @args;
+for my $arg (@cmdline_args) {
+    if ($arg eq '--clean') {
+        clean();
+    } elsif ($arg =~ m/--with-distro=(.*)$/) {
+        my $config = "distro-configs/$1.conf";
+        if (! -f $config) {
+            invalid_distro ($config, $1);
+            } else {
+                push @args, read_args ($config);
+            }
+        } else {
+        push @args, $arg;
+    }
+}
+for my $arg (@args) {
+    if ($arg =~ /^([A-Z]+)=(.*)/) {
+        $ENV{$1} = $2;
+    }
+}
+
+# Alloc $ACLOCAL to specify which aclocal to use
+$aclocal = $ENV{ACLOCAL} ? $ENV{ACLOCAL} : 'aclocal';
+
+my $system = `uname -s`;
+chomp $system;
+
+sanity_checks ($system) unless($system eq 'Darwin');
+
+my $aclocal_flags = $ENV{ACLOCAL_FLAGS};
+
+$aclocal_flags = "-I ./m4/mac" if (($aclocal_flags eq "") && ($system eq 'Darwin'));
+
+$ENV{AUTOMAKE_EXTRA_FLAGS} = '--warnings=no-portability' if (!($system eq 'Darwin'));
+
+system ("$aclocal $aclocal_flags") && die "Failed to run aclocal";
+unlink ("configure");
+system ("autoconf") && die "Failed to run autoconf";
+die "failed to generate configure" if (! -x "configure");
+
+if (defined $ENV{NOCONFIGURE}) {
+    print "Skipping configure process.";
+} else {
+    # Save autogen.lastrun only if we did get some arguments on the command-line
+    if (@ARGV) {
+        if ($#cmdline_args > 0) {
+            # print "writing args to autogen.lastrun\n";
+            my $fh;
+            open ($fh, ">autogen.lastrun") || die "can't open autogen.lastrun: $!";
+            for my $arg (@cmdline_args) {
+                print $fh "$arg\n";
+            }
+            close ($fh);
+        }
+    }
+    print "running ./configure with '" . join ("' '", @args), "'\n";
+    system ("./configure", @args) && die "Error running configure";
+}
+
+# Local Variables:
+# mode: perl
+# cperl-indent-level: 4
+# tab-width: 4
+# indent-tabs-mode: nil
+# End:
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab: #
