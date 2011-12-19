e52547d8c2e59f3a29a988d2cfe78db52343ac4d
hurzl <martin.dieringer@gmx.de>
2011-12-19 07:24:55 +0100

    more tidy up

--- src/slicer/cuttingplane.cpp
+++ src/slicer/cuttingplane.cpp
@@ -24,6 +24,132 @@
 #include "slicer.h"
 #include "cuttingplane.h"
 
+
+bool InFillHitCompareFunc(const InFillHit& d1, const InFillHit& d2)
+{
+	return d1.d < d2.d;
+}
+
+// calculates intersection and checks for parallel lines.
+// also checks that the intersection point is actually on
+// the line segment p1-p2
+bool IntersectXY(const Vector2d &p1, const Vector2d &p2, const Vector2d &p3, const Vector2d &p4, InFillHit &hit)
+{
+	// BBOX test
+	if(MIN(p1.x,p2.x) > MAX(p3.x,p4.x))
+		return false;
+	if(MAX(p1.x,p2.x) < MIN(p3.x,p4.x))
+		return false;
+	if(MIN(p1.y,p2.y) > MAX(p3.y,p4.y))
+		return false;
+	if(MAX(p1.y,p2.y) < MIN(p3.y,p4.y))
+		return false;
+
+
+	if(ABS(p1.x-p3.x) < 0.01 && ABS(p1.y - p3.y) < 0.01)
+	{
+		hit.p = p1;
+		hit.d = sqrt( (p1.x-hit.p.x) * (p1.x-hit.p.x) + (p1.y-hit.p.y) * (p1.y-hit.p.y));
+		hit.t = 0.0;
+		return true;
+	}
+	if(ABS(p2.x-p3.x) < 0.01 && ABS(p2.y - p3.y) < 0.01)
+	{
+		hit.p = p2;
+		hit.d = sqrt( (p1.x-hit.p.x) * (p1.x-hit.p.x) + (p1.y-hit.p.y) * (p1.y-hit.p.y));
+		hit.t = 1.0;
+		return true;
+	}
+	if(ABS(p1.x-p4.x) < 0.01 && ABS(p1.y - p4.y) < 0.01)
+	{
+		hit.p = p1;
+		hit.d = sqrt( (p1.x-hit.p.x) * (p1.x-hit.p.x) + (p1.y-hit.p.y) * (p1.y-hit.p.y));
+		hit.t = 0.0;
+		return true;
+	}
+	if(ABS(p2.x-p4.x) < 0.01 && ABS(p2.y - p4.y) < 0.01)
+	{
+		hit.p = p2;
+		hit.d = sqrt( (p1.x-hit.p.x) * (p1.x-hit.p.x) + (p1.y-hit.p.y) * (p1.y-hit.p.y));
+		hit.t = 1.0;
+		return true;
+	}
+
+	InFillHit hit2;
+	double t0,t1;
+	if(intersect2D_Segments(p1,p2,p3,p4,hit.p, hit2.p, t0,t1) == 1)
+	{
+	hit.d = sqrt( (p1.x-hit.p.x) * (p1.x-hit.p.x) + (p1.y-hit.p.y) * (p1.y-hit.p.y));
+	hit.t = t0;
+	return true;
+	}
+	return false;
+/*
+
+  float xD1,yD1,xD2,yD2,xD3,yD3;
+  float dot,deg,len1,len2;
+  float segmentLen1,segmentLen2;
+  float ua,ub,div;
+
+  // calculate differences
+  xD1=p2.x-p1.x;
+  xD2=p4.x-p3.x;
+  yD1=p2.y-p1.y;
+  yD2=p4.y-p3.y;
+  xD3=p1.x-p3.x;
+  yD3=p1.y-p3.y;
+
+  // calculate the lengths of the two lines
+  len1=sqrt(xD1*xD1+yD1*yD1);
+  len2=sqrt(xD2*xD2+yD2*yD2);
+
+  // calculate angle between the two lines.
+  dot=(xD1*xD2+yD1*yD2); // dot product
+  deg=dot/(len1*len2);
+
+  // if ABS(angle)==1 then the lines are parallel,
+  // so no intersection is possible
+  if(ABS(deg)==1)
+	  return false;
+
+  // find intersection Pt between two lines
+  hit.p=Vector2f (0,0);
+  div=yD2*xD1-xD2*yD1;
+  ua=(xD2*yD3-yD2*xD3)/div;
+  ub=(xD1*yD3-yD1*xD3)/div;
+  hit.p.x=p1.x+ua*xD1;
+  hit.p.y=p1.y+ua*yD1;
+
+  // calculate the combined length of the two segments
+  // between Pt-p1 and Pt-p2
+  xD1=hit.p.x-p1.x;
+  xD2=hit.p.x-p2.x;
+  yD1=hit.p.y-p1.y;
+  yD2=hit.p.y-p2.y;
+  segmentLen1=sqrt(xD1*xD1+yD1*yD1)+sqrt(xD2*xD2+yD2*yD2);
+
+  // calculate the combined length of the two segments
+  // between Pt-p3 and Pt-p4
+  xD1=hit.p.x-p3.x;
+  xD2=hit.p.x-p4.x;
+  yD1=hit.p.y-p3.y;
+  yD2=hit.p.y-p4.y;
+  segmentLen2=sqrt(xD1*xD1+yD1*yD1)+sqrt(xD2*xD2+yD2*yD2);
+
+  // if the lengths of both sets of segments are the same as
+  // the lenghts of the two lines the point is actually
+  // on the line segment.
+
+  // if the point isn't on the line, return null
+  if(ABS(len1-segmentLen1)>0.00 || ABS(len2-segmentLen2)>0.00)
+    return false;
+
+  hit.d = segmentLen1-segmentLen2;
+  return true;*/
+}
+
+
+
 CuttingPlane::CuttingPlane()
 {
 }
