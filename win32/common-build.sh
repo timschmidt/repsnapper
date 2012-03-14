c5f873d97625d66ea7953e9762c6943dc809e81f
Jan Schmidt <thaytan@noraisin.net>
2012-03-14 13:45:52 +1100

    win32: Add jhbuild based build scripts for win32 cross-compile

---
+++ win32/common-build.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+if [ -z "$BUILD_FLAVOUR" ]; then exit 0; fi
+
+export TARGET=target.$BUILD_FLAVOUR
+export CHECKOUT=checkout.$BUILD_FLAVOUR
+
+jhbuild --file=gtk+-win32.jhbuildrc "$@" || exit 1
+
+if [ ! -d "$TARGET/lib/gdk-pixbuf-2.0/2.10.0/" ]; then
+  mkdir -p "$TARGET/lib/gdk-pixbuf-2.0/2.10.0/"
+fi
+
+if [ -f "$TARGET/bin/gdk-pixbuf-query-loaders.exe" ]; then
+  wine "$TARGET/bin/gdk-pixbuf-query-loaders.exe" > "$TARGET/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache"
+fi
+
+# Copy GLUT files into place
+GLUTVER=3.7.6
+
+cd "$CHECKOUT" && wget -c http://user.xmission.com/~nate/glut/glut-$GLUTVER-bin.zip -O glut-$GLUTVER-bin.zip &&
+unzip -f glut-$GLUTVER-bin.zip && cd .. || exit 1
+
+mkdir -p "$TARGET"/include/GL/
+cp "$CHECKOUT"/glut-$GLUTVER-bin/glut.h "$TARGET"/include/GL/
+cp "$CHECKOUT"/glut-$GLUTVER-bin/glut32.dll "$TARGET"/bin/
+cp "$CHECKOUT"/glut-$GLUTVER-bin/glut32.lib "$TARGET"/lib/
+
+# Build repsnapper
+if [ ! -d "$CHECKOUT/repsnapper" ]; then
+  mkdir "$CHECKOUT"/repsnapper
+fi
+cd "$CHECKOUT"/repsnapper
+CPPFLAGS=-I../../"$TARGET"/include PKG_CONFIG_PATH=../../"$TARGET"/lib/pkgconfig ../../../configure --prefix="`pwd`/../../$TARGET" --host=i586-mingw32msvc && make -j4 && make install || exit 1
+cd ../..
+
+# Finally, copy appropriate libgcc into place
+GCCPATH=`i586-mingw32msvc-gcc -print-libgcc-file-name`
+GCCPATH=`dirname "$GCCPATH"`
+for f in $GCCPATH/libgcc*dll; do
+  cp "$f" "$TARGET/bin"
+done
+
