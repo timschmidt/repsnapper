e8cd3aa7e8b1e591a070874bddbf60f524563b7b
hurzl <martin.dieringer@gmx.de>
2012-03-21 05:31:28 +0100

    still needed clipperlib emergency workaround

--- libraries/clipper/clipper/polyclipping/trunk/cpp/clipper.cpp
+++ libraries/clipper/clipper/polyclipping/trunk/cpp/clipper.cpp
@@ -1174,11 +1174,19 @@ bool PolySort(OutRec *or1, OutRec *or2)
     else return false;
   }
   int i1, i2;
-    // if (or1->FirstLeft==NULL) return false;
+    if (or1->FirstLeft==NULL) {
+      // std::cerr << or1 << std::endl;
+      // throw 22;
+      return false;
+    }
   if (or1->isHole)
     i1 = or1->FirstLeft->idx; else
     i1 = or1->idx;
-    // if (or2->FirstLeft==NULL) return true;
+    if (or2->FirstLeft==NULL) {
+      // std::cerr << or1 << std::endl;
+      // throw 22;
+      return false;
+    }
   if (or2->isHole)
     i2 = or2->FirstLeft->idx; else
     i2 = or2->idx;
