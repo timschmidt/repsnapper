73f4f11adcd3a96f32eff7435b29b094174454b0
hurzl <martin.dieringer@gmx.de>
2012-04-01 17:55:36 +0200

    test for BSD awk in gitversion.sh
    it needs a backslash before "="

--- src/gitversion.sh
+++ src/gitversion.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
+AWK=awk
+
+FSEQUAL="="
+# test for BSD awk which needs a blackslash:
+if [ "X`$AWK --version | head -n 1 | grep BSD`" != "X" ] ; then
+    FSEQUAL="\="
+fi
+
+
 VERSION_H=gitversion.h
 
 LOG=`git log -1`
 
-COMMIT=`echo "$LOG" | awk '/commit/{print $2; exit}'`
-DATE=`  echo "$LOG" | awk -v FS="Date:   " '/Date:/{print $2; exit}'`
+COMMIT=`echo "$LOG" | $AWK '/commit/{print $2; exit}'`
+DATE=`  echo "$LOG" | $AWK -v FS="Date:   " '/Date:/{print $2; exit}'`
 
 OLDCOMMIT=""
 if [ -e $VERSION_H ]; then
-    OLDCOMMIT=`awk -v FS="=" '/const string GIT_COMMIT=/{print $2}' $VERSION_H`
+    OLDCOMMIT=`$AWK -v FS="$FSEQUAL" '/const string GIT_COMMIT=/{print $2}' $VERSION_H`
 fi
 
 if [ "x\"$COMMIT\";" == "x$OLDCOMMIT" ]; then
