56d535ecc34aca39a98de980a26c3d4d8edfd5ce
Jan Schmidt <thaytan@noraisin.net>
2012-04-04 21:36:51 +1000

    win32: Add dummy serial_setDTR and serial_flipDTR
    Fix the win32 build by adding dummy functions to the serialwin32.c
    file (for now).

--- libraries/libreprap/serialwin32.c
+++ libraries/libreprap/serialwin32.c
@@ -6,3 +6,13 @@ serial_open(const char *path, long speed, char **error) {
 int
 serial_close(Serial fd) {
 }
+
+int
+serial_setDTR(Serial fd, short dtr)
+{
+}
+
+int
+serial_flipDTR(Serial fd)
+{
+}
