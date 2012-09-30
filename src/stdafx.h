43b43604c1b23e70c027ea8c31ff1c83cc72dbc1
hurzl <martin.dieringer@gmx.de>
2012-09-30 18:13:20 +0200

    include config.h in stdafx.h

--- src/stdafx.h
+++ src/stdafx.h
@@ -25,6 +25,8 @@
 #ifndef STDAFX_H
 #define STDAFX_H
 
+#include "config.h"
+
 #ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.
 #define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
 #endif
