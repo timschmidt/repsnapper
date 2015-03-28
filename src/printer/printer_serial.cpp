fb8897a23810273921c8840804a8b5e853a2bb7e
hurzl <hurzl@online.de>
2015-03-28 13:49:06 +0100

    reformat

--- src/printer/printer_serial.cpp
+++ src/printer/printer_serial.cpp
@@ -1,20 +1,20 @@
 /*
-    This file is a part of the RepSnapper project.
-    Copyright (C) 2011-12 martin.dieringer@gmx.de
+  This file is a part of the RepSnapper project.
+  Copyright (C) 2011-12 martin.dieringer@gmx.de
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+  You should have received a copy of the GNU Lesser General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
 #ifdef HAVE_CONFIG_H
@@ -273,10 +273,10 @@ bool PrinterSerial::RawConnect( string device, int baudrate ) {
   case 115200: speed = B115200; break;
   case 230400: speed = B230400; break;
   default:
-	ostringstream ostr;
-	ostr << "Nonstandard baudrate " << baudrate << endl;
-	LogLine( ostr.str().c_str() );
-	speed = baudrate;
+    ostringstream ostr;
+    ostr << "Nonstandard baudrate " << baudrate << endl;
+    LogLine( ostr.str().c_str() );
+    speed = baudrate;
     // char err_str[ 256 ];
     // snprintf( err_str, 256, _( %d\n"), baudrate );
     // if ( err_str[ 254 ] != '\0' )
--- src/printer/threaded_printer_serial.h
+++ src/printer/threaded_printer_serial.h
@@ -1,20 +1,20 @@
 /*
-    This file is a part of the RepSnapper project.
-    Copyright (C) 2011-12 martin.dieringer@gmx.de
+  This file is a part of the RepSnapper project.
+  Copyright (C) 2011-12 martin.dieringer@gmx.de
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+  You should have received a copy of the GNU Lesser General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
 #pragma once
