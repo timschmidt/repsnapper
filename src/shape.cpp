0862dddc286fc21d061eb6fcbbdbe64332eccc0f
Korneliusz Osmenda <korneliuszo@gmail.com>
2016-06-08 23:34:59 +0200

    Reserve memory so later push_back doesn't make allocations

--- src/shape.cpp
+++ src/shape.cpp
@@ -363,6 +363,7 @@ vector<Vector3d> Shape::getMostUsedNormals() const
   // vector<double> area;
   uint ntr = triangles.size();
   vector<bool> done(ntr);
+  normals.reserve(ntr);
   for(size_t i=0;i<ntr;i++) done[i] = false;
   for(size_t i=0;i<ntr;i++)
     {
